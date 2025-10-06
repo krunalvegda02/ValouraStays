bfb081bbb47018f26310d955248985c246977de3
Krunal Vegda <Krunal.vegda02@gmail.com>
2025-10-06 13:33:49 +0530

    Fix import case for FullSection component in VillaIzara

--- src/Pages/VillaIzara/VillaIzara.jsx
+++ src/Pages/VillaIzara/VillaIzara.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroIzara from './HeroIzara'
 import Navbar2 from '../../layout/Navbar2'
-import FullSection from './fullSection'
+import FullSection from './FullSection'
 import SendMessage from '../../components/SendMessage'
 
 const VillaIzara = () => {
